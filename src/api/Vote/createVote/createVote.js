e23cd954bbec90f5f66065ce28ddf83ac3655f95
Doo Lee <dlengjs12@naver.com>
2020-12-16 08:32:44 +0900

    createVote 로직 수정

--- src/api/Vote/createVote/createVote.js
+++ src/api/Vote/createVote/createVote.js
@@ -2,12 +2,78 @@ const { PrismaClient } = require("@prisma/client")
 const prisma = new PrismaClient()
 export default {
     Mutation:{
-        createUser: async (_, args) =>{
-            const { username, email} = args;
-            const user = await prisma.user.create({
+        createVote: async (_, args) =>{
+            const { 
+                id,
+                title,
+                description,
+                users,
+                upperVote,
+                lowerVote,
+                val1,
+                val2,
+                val3,
+                val4,
+                val5,
+                val6,
+                val7,
+                val8,
+                val9,
+                val10,
+                val11,
+                val12,
+                val13,
+                val14,
+                val15,
+                val16,
+                val17,
+                val18,
+                val19,
+                val20
+            } = args;
+
+            const Vote = await prisma.vote.create({
                 data:{
-                    username:username,
-                    email:email
+                    id:id,
+                    title:title,
+                    description:description,
+
+                    users: users == undefined ? undefined : {
+                        connect: {
+                            id: users,
+                        }
+                    },
+                    upperVote : upperVote == undefined ? undefined : {
+                        connect: {
+                            id: upperVote,
+                        }
+                    },
+                    lowerVote: lowerVote == undefined ? undefined : {
+                        connect: {
+                            id: lowerVote,
+                        }
+                    },
+
+                    val1:val1,
+                    val2:val2,
+                    val3:val3,
+                    val4:val4,
+                    val5:val5,
+                    val6:val6,
+                    val7:val7,
+                    val8:val8,
+                    val9:val9,
+                    val10:val10,
+                    val11:val11,
+                    val12:val12,
+                    val13:val13,
+                    val14:val14,
+                    val15:val15,
+                    val16:val16,
+                    val17:val17,
+                    val18:val18,
+                    val19:val19,
+                    val20:val20
                 }
             });
             // return user; 20201006 return user 시 데이터 에러 발생함(boolean 값이어야 함)
